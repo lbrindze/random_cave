c06cd019c4dbe5111773105c87b816861bd1d817
Loren Brindze <lorenbrindze@gmail.com>
2019-09-30 00:27:39 -0700

    README and cli args

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a procedural cave generator.  It turned out to work suprisingly well given its very simple implementation.
+To run just type in `python cave_maker.py CAVE_HEIGHT CAVE_LENGTH MIN_CAVE_GAP`.
---
+++ cave_maker.py
@@ -0,0 +1,113 @@
+import curses
+import sys
+import time
+from random import random
+
+
+class CaveSlice:
+    def __init__(self, max_height, min_opening, ceiling=None, floor=0):
+        self.max_height = max_height
+        self.min_opening = min_opening
+        self.ceiling = max_height - 1
+        if ceiling is not None:
+            self.ceiling = ceiling
+        self.floor = floor
+
+    def build_slice(self):
+        out = []
+        out.append("*")
+        for i in reversed(range(self.max_height)):
+            if i == self.ceiling or i == self.floor:
+                out.append("_")
+            else:
+                out.append(" ")
+
+        out.append("*")
+        return out
+
+    def _at_min_opening(self):
+        return (self.ceiling - self.floor) == self.min_opening
+
+    def raise_ceiling(self):
+        if self.ceiling < self.max_height - 1:
+            self.ceiling += 1
+
+    def lower_ceiling(self):
+        if not self._at_min_opening():
+            self.ceiling -= 1
+
+    def raise_floor(self):
+        if not self._at_min_opening():
+            self.floor += 1
+
+    def lower_floor(self):
+        if self.floor > 0:
+            self.floor -= 1
+
+    def __str__(self):
+        cave_slice = self.build_slice()
+        return "\n".join(cave_slice)
+
+
+class Cave:
+    def __init__(self, max_height=10, width=10, min_opening=3):
+        self.max_height = max_height
+        self.min_opening = min_opening
+        self.cave_buffer = [self.init_cave_slice() for _ in range(width)]
+
+    def init_cave_slice(self):
+        return CaveSlice(self.max_height, self.min_opening)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        del self.cave_buffer[0]
+        last_slice = self.cave_buffer[-1]
+        next_slice = CaveSlice(
+            last_slice.max_height,
+            last_slice.min_opening,
+            ceiling=last_slice.ceiling,
+            floor=last_slice.floor,
+        )
+
+        ceiling = random()
+        if ceiling > 0.5:
+            next_slice.raise_ceiling()
+        else:
+            next_slice.lower_ceiling()
+
+        floor = random()
+        if floor > 0.5:
+            next_slice.raise_floor()
+        else:
+            next_slice.lower_floor()
+
+        self.cave_buffer.append(next_slice)
+        return self
+
+    def __str__(self):
+        flipped = [cave_slice.build_slice() for cave_slice in self.cave_buffer]
+        out = list(map(list, zip(*flipped)))
+        return "\n".join(["".join(line) for line in out])
+
+
+def main(stdscr, height, length, min_opening):
+    curses.noecho()
+    curses.cbreak()
+
+    cave = Cave(height, length, min_opening)
+    while True:
+        stdscr.clear()
+        stdscr.addstr(str(next(cave)))
+        stdscr.refresh()
+        time.sleep(0.1)
+
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+
+
+if __name__ == "__main__":
+    args = [int(x) for x in sys.argv[1:]]
+    curses.wrapper(main, *args)
